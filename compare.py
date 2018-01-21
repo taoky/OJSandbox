fd35b56697ad0af0ff9cbff2ff1cb95b1f62d551
iBug <7273074+iBug@users.noreply.github.com>
2018-01-21 13:30:47 +0800

    Add command line for file comparison

--- compare.py
+++ compare.py
@@ -1,4 +1,12 @@
+import os
+import sys
 import filecmp
 
 def strictCompare(file1, file2):
     return filecmp.cmp(file1, file2, shallow=False)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        if os.path.isfile(sys.argv[1]) and os.path.isfile(sys.argv[2]):
+            print(strictCompare(sys.argv[1], sys.argv[2]))
