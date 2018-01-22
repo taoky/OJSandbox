10e747636375904319ae054275a7f40ed4fca8c3
iBug <7273074+iBug@users.noreply.github.com>
2018-01-22 21:51:21 +0800

    Fully working file comparison

--- compare.py
+++ compare.py
@@ -1,12 +1,27 @@
+#!/usr/bin/env python3
 import os
 import sys
-import filecmp
+
+def trim(l):
+    while l[-1] == '':
+        l.pop()
+    return l
 
 def strictCompare(file1, file2):
-    return filecmp.cmp(file1, file2, shallow=False)
+    with open(file1, 'r') as a, open(file2, 'r') as b:
+        linesA, linesB = trim([l.rstrip() for l in a]), trim([l.rstrip() for l in b])
+        return linesA == linesB
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        if os.path.isfile(sys.argv[1]) and os.path.isfile(sys.argv[2]):
-            print(strictCompare(sys.argv[1], sys.argv[2]))
+    try:
+        if len(sys.argv) == 3:
+            if os.path.isfile(sys.argv[1]) and os.path.isfile(sys.argv[2]):
+                print(strictCompare(sys.argv[1], sys.argv[2]))
+            else:
+                raise ValueError('Not files')
+        else:
+            raise ValueError('Bad arguments')
+    except ValueError as e:
+        sys.exit(e)
+
