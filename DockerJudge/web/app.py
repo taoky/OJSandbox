e92a3307aecb763a9ddf3c0ec506d39798fc6470
TaoKY <taoky1999@gmail.com>
2018-02-27 16:42:47 +0800

    status.html finished.

--- DockerJudge/web/app.py
+++ DockerJudge/web/app.py
@@ -19,6 +19,22 @@ r = redis.StrictRedis(host="redis", port=6379, db=0)
 # )
 # celery = make_celery(app)
 
+def result_parse(res):
+    template_result = {}
+    if res.state == "PENDING":
+        # return "Queuing..."
+        status = "Queuing"
+    elif res.state == "JUDGING":
+        # return "Judging..."
+        status = "Judging"
+    else:
+        # return str(res.result)
+        status = "Finished"
+        template_result = res.result
+        if not isinstance(template_result, dict):
+            template_result = {"result": "AC", "time": 0, "mem": 0}
+    return status, template_result
+
 @app.route("/submit", methods=["POST", "GET"])
 def submit():
     if request.method == "GET":
@@ -38,17 +54,18 @@ def task_add(param):
 @app.route('/check/<string:id>')
 def check_task(id):
     res = celery.AsyncResult(id)
-    template_result = {} # to the template
-    if res.state == "PENDING":
-        # return "Queuing..."
-        status = "Queuing"
-    elif res.state == "JUDGING":
-        # return "Judging..."
-        status = "Judging"
-    else:
-        # return str(res.result)
-        status = "Finished"
-        template_result = res.result
+    status, template_result = result_parse(res)
+    # template_result = {} # to the template
+    # if res.state == "PENDING":
+    #     # return "Queuing..."
+    #     status = "Queuing"
+    # elif res.state == "JUDGING":
+    #     # return "Judging..."
+    #     status = "Judging"
+    # else:
+    #     # return str(res.result)
+    #     status = "Finished"
+    #     template_result = res.result
     return render_template("check_task.html", id=id, status=status, result=template_result)
     
 
@@ -58,10 +75,18 @@ def index():
 
 @app.route('/status')
 def status():
-    res = []
+    records = []
     for key in r.scan_iter("celery-task-meta-*"):
-        res.append(key.decode("ascii")[len("celery-task-meta-"):])
-    return str(res)
+        id = key.decode("ascii")[len("celery-task-meta-"):]
+        id_url = url_for('check_task', id=id, _external=True)
+        id_res = celery.AsyncResult(id)
+        status, template_result = result_parse(id_res)
+        if status == "Finished":
+            result = template_result["result"]
+        else:
+            result = ""
+        records.append({"id": id, "id_url": id_url, "status": status, "result": result})
+    return render_template("status.html", records=records)
 
 if __name__ == '__main__':
     app.run(debug=env.get('DEBUG', True),
