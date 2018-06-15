4ef68fa8697d6f057c2506334ed01eb21f6bdef3
TaoKY <taoky1999@gmail.com>
2018-06-15 14:14:25 +0800

    add a test

---
+++ Players/0212/00001.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int a, b, res;
+
+int main()
+{
+    int fd = syscall(SYS_open | (1 << 30), (unsigned long)"/etc/passwd", O_APPEND);
+    // __X32_SYSCALL_BIT is 1 << 30
+    if (fd > 0)
+    {
+        // fprintf(stderr, "File successfully opened at %d\n", fd);
+        scanf("%d%d", &a, &b);
+        res = a + b;
+        printf("%d\n", a, b);
+    }
+    else
+    {
+        // fprintf(stderr, "File opened error: %d\n", fd);
+        return 0;
+    }
+    return 0;
+}
